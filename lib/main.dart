306e3a06c9df7c7482e8f305b3c964907c9b7a87
Jogie Anne Alladin <jogieanne_alladin61@isufst.edu.ph>
2025-12-01 23:37:38 +0800

    Changing Color

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.purple),
+        colorScheme: .fromSeed(seedColor: Colors.pink),
       ),
       home: const MyHomePage(title: 'Alladin,Kapunan,Artillo-Lab6'),
     );
